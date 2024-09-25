c43ae03c7510800a4e5487889b6e2f1ac9d27a5b
Reycko <78082869+Reycko@users.noreply.github.com>
2024-09-25 15:35:44 +0200

    refactor: turn consts.VERSION into a semver object

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
-import 'semver';
+import * as semver from 'semver';
 import { version } from '$/package.json';
 
 /* Constants for stuff like version */
 
-export const VERSION: string = version;
+export const VERSION: semver.SemVer | string = semver.parse(version) ?? version;
